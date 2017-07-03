53f5339b267468c6eedfb7525bd176156a2af79e
marprin <marprin93@gmail.com>
2017-07-03 18:23:27 +0700

    update user model and routes

--- Model/User.js
+++ Model/User.js
@@ -1,7 +1,29 @@
 'use strict';
 
-module.exports = (params) => {
-    return {
-        
-    }
+module.exports = ({ Sequelize, sequelizeClient } = params) => {
+    let user = sequelizeClient.define('users', {
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            get() {
+                return this.getDataValue('name').toUpperCase();
+            }
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            },
+            set(val) {
+                this.setDataValue('email', val.toLowerCase());
+            }
+        }
+    }, {
+        underscored: true,
+        timestamps: true,
+        paranoid: true
+    });
+
+    return user;
 }
--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,6 @@
 'use strict';
 
-module.exports = (params) => {
-    const app = params.app;
-    const Middleware = params.Middleware;
-    const Controller = params.Controller;
-
+module.exports = ({app, Controller, Middleware, Model} = params) => {
     app.get('', Controller.WelcomeController.index);
     app.get('/login', Controller.LoginController.index);
 }
