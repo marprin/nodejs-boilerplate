31005b18cb68451c7803e724b2493c8f4f0cb237
marprin <marprin93@gmail.com>
2017-12-08 10:53:27 +0700

    Add new function on helper to decrypt and encrypt

--- helper/helper.js
+++ helper/helper.js
@@ -1,10 +1,24 @@
 'use strict';
 
-module.exports = (params) => {
+module.exports = ({Crypto, env} = params) => {
     return {
         notFoundResponse: (res, status = 404, error = {}, message = 'Not Found', title = 'Not Found') => {
             res.status(status);
             return res.render('error.html', {error, message, title});
-        }
+        },
+        textEncryption: (value) => {
+            return Crypto.AES.encrypt(value, env.APP_ENCRYPT_KEY);
+        },
+        textDecryption: (encryptedValue) => {
+            let bytes = Crypto.AES.decrypt(encryptedValue.toString(), env.APP_ENCRYPT_KEY);
+            return bytes.toString(Crypto.enc.Utf8);
+        },
+        objectEncryption: (data) => {
+            return Crypto.AES.encrypt(JSON.stringify(data), env.APP_ENCRYPT_KEY);
+        },
+        decryptToObject: (encryptedValue) => {
+            let bytes = Crypto.AES.decrypt(encryptedValue.toString(), env.APP_ENCRYPT_KEY);
+            return JSON.parse(bytes.toString(Crypto.enc.Utf8));
+        },
     }
 }
